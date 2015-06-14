957ba852d2ba81e70a404f18d123200e2bf3f99c
audrie <audrie.talbot@gmail.com>
2015-06-14 16:48:02 +0000

    Passes spec for save and eql methods for museum class.

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -16,4 +16,20 @@ describe(Museum) do
       expect(Museum.all()).to(eq([]))
     end
   end
+  
+  describe('#save') do
+    it('adds a museum to the database') do
+      test_museum = Museum.new({:name => 'Portland Museum', :id => nil})
+      test_museum.save()
+      expect(Museum.all()).to(eq([test_museum]))
+    end
+  end
+  
+  describe('#==') do
+    it('recognizes that two museums that have the same name and ID are equal') do
+      test_museum = Museum.new({:name => 'Portland Museum', :id => nil})
+      test_museum2 = Museum.new({:name => 'Portland Museum', :id => nil})
+      expect(test_museum).to(eq(test_museum2))
+    end
+  end
 end
