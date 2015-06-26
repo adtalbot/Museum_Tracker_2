3563e51a2c4483c15e1d441666aab5285a61f751
audrie <audrie.talbot@gmail.com>
2015-06-26 18:26:12 +0000

    All routes functioning.

--- app.rb
+++ app.rb
@@ -12,20 +12,32 @@ get('/') do
 end
 
 get('/museums') do
-  @museums = Museum.all()
+  @museums = Museum.all()  
   erb(:museums)
-end
-
-post('/museums') do
-  name = params.fetch('name')
-  musuem = Museum.new({:name => name})
-  museum.save()
-  redirect back
 end
 
 get('/artworks') do
   @artworks = Artwork.all()
   erb(:artworks)
+end
+
+get('/museum') do
+  @museums = Museum.all()
+  erb(:museum)
+end
+
+post('/museum') do
+  name = params.fetch('name')
+  museum_id = params.fetch('museum_id').to_i()
+  artwork = Artwork.new({:name => name, :museum_id => museum_id})
+  artwork.save()
+  @museum = Museum.find(museum_id)
+  redirect back
+end
+
+get('/museums/:id') do
+  @museum = Museum.find(params.fetch('id').to_i())
+  erb(:museum)
 end
 
 
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,13 +1,13 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
 
-# describe('the museum tracker website path', {:type => :feature}) do
-#   it('stores a list of museums and artwork') do
-#     visit('/')
-#     expect(page).to(have_content('Click to see all museums'))
-#     expect(page).to(have_content('Click to see all artwork'))
-#   end
-# end
+describe('the museum tracker website path', {:type => :feature}) do
+  it('stores a list of museums and artwork') do
+    visit('/')
+    expect(page).to(have_content('Click to see all museums'))
+    expect(page).to(have_content('Click to see all artwork'))
+  end
+end
 
