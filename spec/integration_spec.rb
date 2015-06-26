6fb523062842355bfa085ad1f89eca369b7e94b1
audrie <audrie.talbot@gmail.com>
2015-06-26 21:16:23 +0000

    Passes integration spec for museums page.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,10 +4,16 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the museum tracker website path', {:type => :feature}) do
-  it('stores a list of museums and artwork') do
+  it('displays links to lists of museums and artwork') do
     visit('/')
     expect(page).to(have_content('Click to see all Museums'))
     expect(page).to(have_content('Click to see all Artwork'))
+  end
+  it('shows a list of all museums and allows you to add a museum') do
+    visit('/museums')
+    fill_in('name', {:with => 'Portland Museum'})
+    click_button('Add')
+    expect(page).to(have_content('Portland Museum'))
   end
 end
 
